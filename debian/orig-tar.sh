46fc784ffc0210f4f8c61810c76f7e53842acc36
Gasol Wu <gasol.wu@gmail.com>
2012-12-13 15:42:03 +0800

    Add orig-tar.sh

---
+++ debian/orig-tar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+PACKAGE_NAME=scribe
+PACKAGE_VERSION=2.2
+GIT_VERSION=63e4824838
+URL=https://github.com/facebook/scribe/tarball/${GIT_VERSION}
+TAR=${PACKAGE_NAME}-${PACKAGE_VERSION}~git${GIT_VERSION}.tar.gz
+ORIG_TAR=${PACKAGE_NAME}_${PACKAGE_VERSION}~git${GIT_VERSION}.orig.tar.gz
+
+curl -o ../$TAR $URL
+ln -sf $TAR ../$ORIG_TAR
