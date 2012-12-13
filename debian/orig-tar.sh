ed9293fabc73110a60724141622a6e57efe71cbc
Gasol Wu <gasol.wu@gmail.com>
2012-12-13 15:55:27 +0800

    Curl allow redirection

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -9,5 +9,5 @@ URL=https://github.com/facebook/scribe/tarball/${GIT_VERSION}
 TAR=${PACKAGE_NAME}-${PACKAGE_VERSION}~git${GIT_VERSION}.tar.gz
 ORIG_TAR=${PACKAGE_NAME}_${PACKAGE_VERSION}~git${GIT_VERSION}.orig.tar.gz
 
-curl -o ../$TAR $URL
+curl -L -o ../$TAR $URL
 ln -sf $TAR ../$ORIG_TAR
