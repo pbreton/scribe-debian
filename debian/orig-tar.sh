a4a2551a7344b90a6e50cbdb770bf2da33293ad1
Gasol Wu <gasol.wu@gmail.com>
2012-12-14 11:08:41 +0800

    Remove git version in original tar

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -7,7 +7,7 @@ PACKAGE_VERSION=2.2
 GIT_VERSION=63e4824838
 URL=https://github.com/facebook/scribe/tarball/${GIT_VERSION}
 TAR=${PACKAGE_NAME}-${PACKAGE_VERSION}~git${GIT_VERSION}.tar.gz
-ORIG_TAR=${PACKAGE_NAME}_${PACKAGE_VERSION}~git${GIT_VERSION}.orig.tar.gz
+ORIG_TAR=${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz
 
 curl -L -o ../$TAR $URL
 ln -sf $TAR ../$ORIG_TAR
